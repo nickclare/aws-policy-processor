4af8c09571869df810846d761bf9de14825903fb
Nicholas Clare <nick@conjugate.dev>
2025-08-04 14:11:35 +0800

    try to integrate miette for detailed validation errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+
+use aws_policy_processor::{model::PolicyKind, validator::validate_statement};
+use miette::{IntoDiagnostic, Result};
+
+fn main() -> Result<()> {
+    let input = read_input()?;
+    let statement = serde_json::from_str(&input).into_diagnostic()?;
+    validate_statement(&statement, PolicyKind::ServiceControl)
+}
+
+fn read_input() -> Result<String> {
+    let mut stdin = std::io::stdin().lock();
+    let mut buf = Vec::new();
+    stdin.read_to_end(&mut buf).into_diagnostic()?;
+    Ok(String::from_utf8(buf).into_diagnostic()?)
 }
--- src/validator.rs
+++ src/validator.rs
@@ -1,69 +1,181 @@
 //! Basic validation rules for policy / policy statements
 
+use miette::{Diagnostic, Result};
+use thiserror::Error;
+
 use crate::model::*;
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub enum ErrorKind {
-    ResourceNotPermitted,
-    PrincipalNotPermitted,
-    ConditionNotPermitted,
-    InvalidResource(String),
-    InvalidAction(String),
-    NotActionNotPermitted,
+#[derive(Clone, Debug, PartialEq, Eq, Error, Diagnostic)]
+pub struct ValidationError {
+    sid: Option<String>,
+
+    #[related]
+    errors: Vec<ValidationErrorKind>,
 }
 
-#[derive(Clone, Debug)]
-pub struct Diagnostic {
-    pub error: ErrorKind,
-    pub sid: Option<String>,
-}
-
-impl Diagnostic {
-    pub fn for_statement(statement: &Statement, error: ErrorKind) -> Self {
-        Self {
-            error,
-            sid: statement.sid.clone(),
+impl std::fmt::Display for ValidationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.sid {
+            None => write!(f, "validation error."),
+            Some(sid) => write!(f, "validation error in statement `{sid}`."),
         }
     }
 }
 
-impl Statement {
-    pub fn diagnostic(&self, error: ErrorKind) -> Diagnostic {
-        Diagnostic::for_statement(self, error)
+#[derive(Clone, Debug, PartialEq, Eq, Error, Diagnostic)]
+pub enum ValidationErrorKind {
+    #[error("`Condition` not permitted")]
+    ConditionNotPermitted,
+    #[error("Action value '{0}' is invalid")]
+    InvalidAction(String),
+    #[error("Resource value '{0}' is invalid")]
+    InvalidResource(String),
+    #[error("`NotAction` not permitted")]
+    NotActionNotPermitted,
+    #[error("`Principal` not permitted")]
+    PrincipalNotPermitted,
+    #[error("`Resource` not permitted")]
+    ResourceNotPermitted,
+}
+
+trait ValidationRule {
+    fn validate(&self, statement: &Statement) -> Option<ValidationErrorKind>;
+}
+
+struct FnValidationRule(Box<dyn Fn(&Statement) -> Option<ValidationErrorKind>>);
+
+impl ValidationRule for FnValidationRule {
+    fn validate(&self, statement: &Statement) -> Option<ValidationErrorKind> {
+        self.0(statement)
+    }
+}
+
+impl<F> From<F> for FnValidationRule
+where
+    F: Fn(&Statement) -> Option<ValidationErrorKind> + 'static,
+{
+    fn from(value: F) -> Self {
+        FnValidationRule(Box::new(value))
+    }
+}
+
+struct RuleSet {
+    rules: Vec<Box<dyn ValidationRule>>,
+}
+
+impl RuleSet {
+    pub fn new() -> Self {
+        Self { rules: Vec::new() }
+    }
+
+    pub fn append<R: ValidationRule + 'static>(&mut self, rule: R) -> &mut Self {
+        self.rules.push(Box::new(rule));
+        self
+    }
+
+    pub fn append_fn<F: Into<FnValidationRule>>(&mut self, rule: F) -> &mut Self {
+        self.append(rule.into())
+    }
+}
+
+impl Default for RuleSet {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+mod rules {
+    use super::*;
+
+    pub(crate) fn resource_not_permitted(statement: &Statement) -> Option<ValidationErrorKind> {
+        if let Some(ref _resources) = statement.resources {
+            // todo: check its not just a `*`, which may be allowed
+            Some(ValidationErrorKind::ResourceNotPermitted)
+        } else {
+            None
+        }
+    }
+
+    pub(crate) fn not_actions_not_permitted(statement: &Statement) -> Option<ValidationErrorKind> {
+        if matches!(statement.actions, Actions::NotActions(_)) {
+            Some(ValidationErrorKind::NotActionNotPermitted)
+        } else {
+            None
+        }
+    }
+
+    pub(crate) fn condition_not_permitted(statement: &Statement) -> Option<ValidationErrorKind> {
+        if let Some(ref _condition) = statement.condition {
+            Some(ValidationErrorKind::ConditionNotPermitted)
+        } else {
+            None
+        }
     }
 }
 
 /// Validate a single statement, according to some set of rules for its policy type
-pub fn validate_statement(statement: &Statement, kind: PolicyKind) -> Vec<Diagnostic> {
-    todo!()
-}
-
-fn validate_resource_statement(statement: &Statement) -> Vec<Diagnostic> {
-    let mut diagnostics = Vec::new();
-    if statement.resources.is_some() {
-        diagnostics.push(statement.diagnostic(ErrorKind::ResourceNotPermitted))
+pub fn validate_statement(statement: &Statement, kind: PolicyKind) -> Result<()> {
+    match kind {
+        PolicyKind::Iam => todo!(),
+        PolicyKind::Resource => validate_resource_statement(statement),
+        PolicyKind::ServiceControl => validate_service_control_statement(statement),
+        PolicyKind::ResourceControl => todo!(),
     }
-
-    diagnostics
 }
 
-fn validate_service_control_statement(statement: &Statement) -> Vec<Diagnostic> {
-    let mut diagnostics = Vec::new();
-    // Allow statements have a number of limitations
-    if statement.effect == Effect::Allow {
-        if matches!(statement.actions, Actions::NotActions(_)) {
-            diagnostics.push(statement.diagnostic(ErrorKind::NotActionNotPermitted));
-        }
-        if matches!(statement.condition, Some(_)) {
-            diagnostics.push(statement.diagnostic(ErrorKind::ConditionNotPermitted));
-        }
-        // todo: make this allow `"Resource": "*"`
-        if matches!(statement.resources, Some(_)) {
-            diagnostics.push(statement.diagnostic(ErrorKind::ResourceNotPermitted));
+/// Validate the given statement against a [RuleSet]
+fn validate_against_rules(statement: &Statement, rules: &RuleSet) -> Result<()> {
+    let mut errors = Vec::new();
+    for r in &rules.rules {
+        if let Some(error) = r.validate(statement) {
+            errors.push(error);
         }
     }
 
-    diagnostics
+    into_result(errors, &statement.sid)
+}
+
+macro_rules! ruleset {
+    ( $( $r:expr ),*) => {
+        {
+            let mut result = RuleSet::default();
+            $(
+                result.append_fn($r);
+            )*
+            result
+        }
+    };
+}
+
+fn into_result(errors: Vec<ValidationErrorKind>, sid: &Option<String>) -> Result<()> {
+    if !errors.is_empty() {
+        Err(ValidationError {
+            sid: sid.clone(),
+            errors,
+        }
+        .into())
+    } else {
+        Ok(())
+    }
+}
+
+fn validate_resource_statement(statement: &Statement) -> Result<()> {
+    validate_against_rules(statement, &ruleset![rules::resource_not_permitted])
+}
+
+fn validate_service_control_statement(statement: &Statement) -> Result<()> {
+    use rules::*;
+    match statement.effect {
+        Effect::Allow => validate_against_rules(
+            statement,
+            &ruleset![
+                not_actions_not_permitted,
+                condition_not_permitted,
+                resource_not_permitted
+            ],
+        ),
+        Effect::Deny => Ok(()),
+    }
 }
 
 #[cfg(test)]
@@ -87,17 +199,22 @@ mod tests {
             }                    
         "#};
         let statement: Statement = serde_json::from_str(invalid_policy)?;
-        let diagnostics = validate_service_control_statement(&statement);
+        if let Err(err) = validate_service_control_statement(&statement) {
+            let err = err.downcast::<ValidationError>().unwrap();
+            assert_eq!(err.sid, Some("WeirdStatement".into()));
+            let diagnostics = err.errors;
 
-        let error_of_kind = |kind: ErrorKind| diagnostics.iter().find(|e| e.error == kind);
-        assert!(
-            error_of_kind(ErrorKind::NotActionNotPermitted).is_some(),
-            "expected an error that NotAction is not allowed"
-        );
-        assert!(
-            error_of_kind(ErrorKind::PrincipalNotPermitted).is_none(),
-            "didn't expect a PrincipalNotPermitted error"
-        );
+            let error_of_kind =
+                |kind: ValidationErrorKind| diagnostics.iter().find(|e| **e == kind);
+            assert!(
+                error_of_kind(ValidationErrorKind::NotActionNotPermitted).is_some(),
+                "expected an error that NotAction is not allowed"
+            );
+            assert!(
+                error_of_kind(ValidationErrorKind::PrincipalNotPermitted).is_none(),
+                "didn't expect a PrincipalNotPermitted error"
+            );
+        }
 
         Ok(())
     }
